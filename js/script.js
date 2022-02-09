462d53cf4fe360e9bae7d12587aac9f5fc8ed9ac
Glitch (woozy-thoracic-clock) <none>
2022-02-09 23:22:16 +0000

    🛴📂 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Complete Day 1 goals here
-// store your song titles, artist name, length, image and ink 
+// store your song titles, artist name, length, image and ink
 
 let songTitles = ["DAYDREAM", "Zehnaseeb", "Every Summertime"];
 console.log(songTitles);
@@ -10,20 +10,58 @@ console.log(artistNames);
 let songLength = ["3:43", "2:23", "3:22"];
 console.log(songLength);
 
-
-let songImg = ["https://i.scdn.co/image/ab67616d0000b273744174f599e2d0d5384c5421", "https://i.scdn.co/image/ab67616d0000b2732025db071cd9dd7e8023e01e", "https://images.genius.com/cd1767554b50e0c83a4234bb9a1b7220.1000x1000x1.png"];
+let songImg = [
+  "https://i.scdn.co/image/ab67616d0000b273744174f599e2d0d5384c5421",
+  "https://i.scdn.co/image/ab67616d0000b2732025db071cd9dd7e8023e01e",
+  "https://images.genius.com/cd1767554b50e0c83a4234bb9a1b7220.1000x1000x1.png",
+];
 console.log(songImg);
 
-
-let songLink = ["https://www.youtube.com/watch?v=9IVzPNWRbYE", "https://www.youtube.com/watch?v=gjtJ4YgHiXY", "https://www.youtube.com/watch?v=a0OHkWX7B-E"];
+let songLink = [
+  "https://www.youtube.com/watch?v=9IVzPNWRbYE",
+  "https://www.youtube.com/watch?v=gjtJ4YgHiXY",
+  "https://www.youtube.com/watch?v=a0OHkWX7B-E",
+];
 console.log(songLink);
 
 
-
-
+// Complete the Day 2 goals inside this function
 
 function displaySongInfo() {
-  // Complete the Day 2 goals inside this function
+  let songNames = ["DAYDREAM", "Zehnaseeb", "Every Summertime"];
+  $(".songNames").click(function () {
+    songNames.forEach(function (music) {
+      $(".songs").append("<li>" + music + "</li>");
+    });
+  });
+
+  let artistNamess = ["John OFA Rhee", "Chinmayi, Shekhar Ravjian", "Niki"];
+  $(".artistNamess").click(function () {
+    artistNamess.forEach(function (music) {
+      $(".artists").append("<li>" + music + "</li>");
+    });
+  });
+
+  let songLengthh = ["3:43", "2:23", "3:22"];
+  $(".songLengthh").click(function () {
+    songLengthh.forEach(function (music) {
+      $(".lengths").append("<li>" + music + "</li>");
+    });
+  });
+
+  let songImages = ["3:43", "2:23", "3:22"];
+  $(".songImages").click(function () {
+    songImages.forEach(function (music) {
+      $(".images").append("<li>" + music + "</li>");
+    });
+  });
+
+  let songLlinks = ["3:43", "2:23", "3:22"];
+  $(".songLlinks").click(function () {
+    songLlinks.forEach(function (music) {
+      $(".links").append("<li>" + music + "</li>");
+    });
+  });
 }
 
 function emptySongInfo() {
